d9ffd8100c5baa1a58cd22a8144064bc762a8856
DataSperling <simon.francis.mrsc@tutanota.de>
2024-01-31 15:41:11 +0100

    included fft imported wav

--- FFT_wave_file.py
+++ FFT_wave_file.py
@@ -8,6 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io.wavfile import write
 from scipy.fft import fft, fftfreq, rfft, rfftfreq
+import wave
 
 SAMPLING_FREQUENCY = 44100;   # frequency in Hz / s-1
 SAMPLE_DURATION  = 4;         # time in s 
@@ -20,8 +21,7 @@ def gen_simple_sinwave(frequency, rate, duration):
   y = np.sin( (np.pi*2) * frequencies)
   return x,y
 
-# step 2 use function to generate mixed sample and save as waveform file
-# intensities modeled as the square of frequency
+# step 2 use function to generate composite sample and save as 16bit waveform file
 _, sig_low = gen_simple_sinwave(150, SAMPLING_FREQUENCY, SAMPLE_DURATION)
 sig_low = sig_low*0.3
 _, sig_med = gen_simple_sinwave(600, SAMPLING_FREQUENCY, SAMPLE_DURATION)
@@ -29,19 +29,32 @@ sig_med = sig_med*0.6
 _, sig_hi  = gen_simple_sinwave(2400, SAMPLING_FREQUENCY, SAMPLE_DURATION)
 sig_hi  = sig_hi*0.9
 sig_comp = sig_low + sig_med + sig_hi
-plt.plot(sig_comp[:1000])
-plt.show()
-write('sig_comp.wav', SAMPLING_FREQUENCY, sig_comp)
+#plt.plot(sig_comp[:1000])
+#plt.show()
+write('sig_comp.wav', SAMPLING_FREQUENCY, sig_comp.astype(np.int16))
 
 
 
 # calculate FFT of generated waveform
 fy = rfft(sig_comp)
 fx = rfftfreq(NO_SAMPLES, 1/SAMPLING_FREQUENCY)
-plt.plot(fx, np.abs(fy))
-plt.show()
+#plt.plot(fx, np.abs(fy))
+#plt.show()
 
 # check by subtraction of the FT's that the algorithmically generated FFT
 # and the FFT from waveform analysis are the same
+wav_sample = wave.open('sig_comp.wav')
+print(" sample rate: ", wav_sample.getframerate(), "\n",
+"number samples: ", wav_sample.getnframes(), "\n",
+"number channels: ", wav_sample.getnchannels(), "\n")
+
+signal_wave = wav_sample.readframes(wav_sample.getnframes())
+signal_array = np.frombuffer(signal_wave, dtype=np.int16)
+fy = rfft(signal_array)
+fx = rfftfreq(wav_sample.getnframes(), 1/wav_sample.getframerate())
+plt.plot(fx, np.abs(fy) )
+plt.show()
+
+
 
 print("EOF---EOF---EOF")
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 
 Repo for EDA of audio samples, choosing FFT / DFT algorithms, dimensionality reduction and feature selection.
 
-FFT_wave_file.py: A python script for determining the aplicability of basic 
-scipy.fft and scipy.fftfreq methods for the anlysis and feature extraction in 
+FFT_wave_file.py: A python script for determining the applicability of basic 
+scipy.fft and scipy.fftfreq methods for the analysis and feature extraction in 
 real-world (high-white-noise) sound scenes
 
 FFT-xeno-canto-82416.ipynb: jupyter workbook showing the filtering and FFT of a
