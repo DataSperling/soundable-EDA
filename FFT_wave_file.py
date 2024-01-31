df3b3dbf4e6817ee9e6e3fed72701216a0db0da2
DataSperling <simon.francis.mrsc@tutanota.de>
2024-01-31 14:17:33 +0100

    added python test file for scipy.fft sub

---
+++ FFT_wave_file.py
@@ -0,0 +1,14 @@
+"""
+script to test the methods scipy.fft, fft fftfreq, rfft and rfftfreq produce
+consistent (normalisable) output when applied to calculated and measured .wav
+type audio
+"""
+
+# step 1 define function for generating a waveform from a given parameters
+
+# step 2 use function to generate mixed sample and save as waveform file
+
+# step 3 calculate FFT of generated waveform 
+
+# step 4 check by subtraction of the FT's that the algorithmically generated FFT
+# and the FFT from waveform analysis are the same
